f232f843ef2528ee65d613e62232f0f34983e759
lubennikovaav <lubennikovaav@gmail.com>
2014-07-31 09:04:20 +0400

    regression test gist_indexonly added

---
+++ src/test/regress/sql/gist_indexonly.sql
@@ -0,0 +1,51 @@
+--
+-- Test Index-only scan plan on GiST indexes
+--
+
+CREATE TABLE gist_tbl (b box, p point);
+
+insert into gist_tbl select box(point(0.05*i, 0.05*i), point(0.05*i, 0.05*i)),
+			 point(0.05*i, 0.05*i) FROM generate_series(0,10000) as i;
+
+
+vacuum analyze;
+
+SET enable_seqscan TO false;
+SET enable_bitmapscan TO false;
+SET enable_indexscan TO false;
+SET enable_indexonlyscan TO true;
+
+-- Check singlecolumn index-only scan for point opclass
+
+CREATE INDEX gist_tbl_point_index ON gist_tbl USING gist (p);
+EXPLAIN (COSTS OFF)
+select p from gist_tbl where p <@ box(point(0,0),  point(100,100)) limit 10;
+
+DROP INDEX gist_tbl_point_index;
+
+-- Check singlecolumn index-only scan for box opclass
+
+CREATE INDEX gist_tbl_box_index ON gist_tbl USING gist (b);
+EXPLAIN (COSTS OFF)
+select b from gist_tbl where b <@ box(point(5,5),  point(6,6)) limit 10;
+
+DROP INDEX gist_tbl_box_index;
+
+-- Check multicolumn indexonlyscan for gist
+
+CREATE INDEX gist_tbl_multi_index ON gist_tbl USING gist (b, p);
+
+EXPLAIN (COSTS OFF)
+select * from gist_tbl where ( (b <@ box(point(5,5),  point(6,6))) and (p <@ box(point(0,0),  point(100,100)))) limit 10;
+
+EXPLAIN (COSTS OFF)
+select * from gist_tbl where ( (b <@ box(point(5,5),  point(6,6))) or (p <@ box(point(0,0),  point(100,100)))) limit 10;
+
+DROP INDEX gist_tbl_multi_index;
+
+RESET enable_seqscan;
+RESET enable_bitmapscan;
+RESET enable_indexscan;
+RESET enable_indexonlyscan;
+
+DROP TABLE gist_tbl;
