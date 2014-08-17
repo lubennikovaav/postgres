21e012a375484bed069c17b06022d63e645075a0
lubennikovaav <lubennikovaav@gmail.com>
2014-08-17 17:53:42 +0400

    regression test gist_indexonly checked

--- src/test/regress/sql/gist_indexonly.sql
+++ src/test/regress/sql/gist_indexonly.sql
@@ -5,7 +5,7 @@
 CREATE TABLE gist_tbl (b box, p point);
 
 insert into gist_tbl select box(point(0.05*i, 0.05*i), point(0.05*i, 0.05*i)),
-			 point(0.05*i, 0.05*i) FROM generate_series(0,10000) as i;
+			 point(0.05*i, 0.05*i) FROM generate_series(0,100000) as i;
 
 
 vacuum analyze;
@@ -19,28 +19,23 @@ SET enable_indexonlyscan TO true;
 
 CREATE INDEX gist_tbl_point_index ON gist_tbl USING gist (p);
 EXPLAIN (COSTS OFF)
-select p from gist_tbl where p <@ box(point(0,0),  point(100,100)) limit 10;
-
+select p from gist_tbl where p <@ box(point(0,0),  point(100,100)) and length(p::text) < 10;
 DROP INDEX gist_tbl_point_index;
 
 -- Check singlecolumn index-only scan for box opclass
 
 CREATE INDEX gist_tbl_box_index ON gist_tbl USING gist (b);
+vacuum analyze;
 EXPLAIN (COSTS OFF)
-select b from gist_tbl where b <@ box(point(5,5),  point(6,6)) limit 10;
-
+select b from gist_tbl where b <@ box(point(5,5),  point(6,6));
 DROP INDEX gist_tbl_box_index;
 
 -- Check multicolumn indexonlyscan for gist
 
 CREATE INDEX gist_tbl_multi_index ON gist_tbl USING gist (b, p);
-
+vacuum analyze;
 EXPLAIN (COSTS OFF)
-select * from gist_tbl where ( (b <@ box(point(5,5),  point(6,6))) and (p <@ box(point(0,0),  point(100,100)))) limit 10;
-
-EXPLAIN (COSTS OFF)
-select * from gist_tbl where ( (b <@ box(point(5,5),  point(6,6))) or (p <@ box(point(0,0),  point(100,100)))) limit 10;
-
+select b, p from gist_tbl where ( (b <@ box(point(5,5),  point(6,6))) and (p <@ box(point(5,5),  point(5.5,5.5))));
 DROP INDEX gist_tbl_multi_index;
 
 RESET enable_seqscan;
